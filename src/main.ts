de5a180b5c3ef65ebd7a78b2df5028b7512e43bc
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-05-05 21:16:01 +0800

    feat: styles

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-26 18:06:25
- * @LastEditTime: 2022-05-05 20:57:09
+ * @LastEditTime: 2022-05-05 21:14:08
  * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /ms-math/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(8087);
+  await app.listen(9000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
